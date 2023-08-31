f70c61d8da503bfe8090d43a0985f04df4ac7e54
Galih Wicaksono <galihwicaksono90@gmail.com>
2023-08-31 10:54:35 +0700

    dsfdf

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,26 @@
-import Image from 'next/image'
-import Button from "@/components/"
+'use client'
+import Head from 'next/head'
+import { useEffect } from "react"
+
+const href = "mylink://about/15"
 
 export default function Home() {
+
+  // useEffect(() => {
+  //   const asdf = setTimeout(() => {
+  //     window.location.assign("https://archlinux.org")
+  //   }, 25);
+  //   window.location.assign("mylink://about/12")
+  //   return () => clearTimeout(asdf)
+  // }, [])
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Button />
+
+      <Head>
+        <meta name="apple-itunes-app" content="app-id=1595530037" key="smart-banner" />
+      </Head>
+      <button onClick={() => console.log('hello wolrd')}>testing</button>
     </main>
   )
 }
